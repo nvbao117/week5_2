7b67e3065b0281b07c1632cbb4c4292000455e32
huy2222 <nguyennhathuy21112005@gmail.com>
2025-12-05 09:03:39 +0700

    finish TourRepository.java and add TourService.java

--- src/main/java/com/example/week5_2/repositories/TourRepository.java
+++ src/main/java/com/example/week5_2/repositories/TourRepository.java
@@ -1,4 +1,9 @@
 package com.example.week5_2.repositories;
 
-public interface TourRepository {
+import com.example.week5_2.entities.Tour;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TourRepository extends JpaRepository<Tour, Long> {
 }
--- src/main/java/com/example/week5_2/service/TourService.java
+++ src/main/java/com/example/week5_2/service/TourService.java
@@ -1,4 +1,12 @@
 package com.example.week5_2.service;
 
+
+import com.example.week5_2.repositories.TourRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TourService {
+    @Autowired
+    private TourRepository tourRepository;
 }
